52bcedbf9d48d1a84f6f7d3fdaf9aa389117a5c0
Andrew Blake <drewblake1974@gmail.com>
2018-10-14 22:18:04 -0400

    added more tests

--- src/components/game.test.js
+++ src/components/game.test.js
@@ -7,9 +7,36 @@ describe('<Game />', () => {
     it('Renders without crashing', () => {
         shallow(<Game />);
     });
-    it(' element', () => {
+    it('Restarts game', () => {
         const wrapper = shallow(<Game />);
+        wrapper.setState({
+            guesses: [55, 22],
+            feedback: 'Good job',
+            correctAnswer: 99
+        });
+        wrapper.instance().restartGame();
         expect(wrapper.state('guesses')).toEqual([]);
         expect(wrapper.state('correctAnswer')).toBeLessThanOrEqual(100);
     });
+    it('Can make Guess', () => {
+        const wrapper = shallow(<Game />);
+        wrapper.setState({
+            correctAnswer: 77
+        });
+        wrapper.instance().makeGuess(75);
+        expect(wrapper.state('guesses')).toEqual([75]);
+        expect(wrapper.state('feedback')).toEqual('You\'re Hot!');
+    });
+    it('Can make aural updates', () => {
+        const wrapper = shallow(<Game />);
+        wrapper.setState({
+            correctAnswer: 88
+        });
+        wrapper.instance().makeGuess(4);
+        wrapper.instance().makeGuess(87);
+        wrapper.instance().generateAuralUpdate();
+        expect(wrapper.state('auralStatus'))
+            .toEqual('Here\'s the status of the game right now: You\'re Hot! You\'ve made 2 guesses. In order of most- to least-recent, they are: 87, 4');
+    });
+
 });
